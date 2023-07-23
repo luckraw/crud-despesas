6a25f41cc0c92f255263c9a534610c1f5033c308
nulldeath <krawdev@gmail.com>
2023-07-23 16:45:34 -0300

    update and delete implement.

--- src/main/java/br/com/nulldeath/dao/DespesaDAO.java
+++ src/main/java/br/com/nulldeath/dao/DespesaDAO.java
@@ -35,12 +35,39 @@ public class DespesaDAO implements IDespesaDAO {
 
     @Override
     public Despesa atualizar(Despesa despesa) {
-        return null;
+        try (Connection connection = ConnectionFactory.getConnection()) {
+            String sql = "UPDATE despesas SET descricao = ?, valor = ?, data = ?, categoria = ? WHERE id = ?;";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, despesa.getDescricao());
+            preparedStatement.setDouble(2, despesa.getValor());
+            preparedStatement.setDate(3, java.sql.Date.valueOf(despesa.getData()));
+            preparedStatement.setString(4, despesa.getCategoria().toString());
+            preparedStatement.setLong(5, despesa.getId());
+
+            preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return despesa;
     }
 
     @Override
     public void deletar(Long id) {
+        try (Connection connection = ConnectionFactory.getConnection()) {
+            String sql = "DELETE from despesas WHERE id = ?";
 
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
